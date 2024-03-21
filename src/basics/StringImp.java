ddf74744a1b82b00372246d68965db7edd6b478d
rampathak <ram.pathak65@gmail.com>
2024-03-21 19:33:56 -0400

    Exception handling code added

--- src/basics/StringImp.java
+++ src/basics/StringImp.java
@@ -4,11 +4,20 @@ public class StringImp {
 
 	public static void main(String[] args) {
 		// split every word from the sentence
+		try {
 		String name = "Hello this is my name";
 		String[] names = name.split(" ");
 		for (String str : names) {
 			System.out.println(str);
+		
 		}
+		}
+		
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		
 	}
 
 }
