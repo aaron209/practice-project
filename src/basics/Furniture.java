d73d4a52b59d82c5e0071684478061a61628fc44
juropani786 <163758390+juropani786@users.noreply.github.com>
2024-03-21 21:58:37 -0400

    Nur modified it a bit

--- src/basics/Furniture.java
+++ src/basics/Furniture.java
@@ -4,12 +4,16 @@ import java.util.Scanner;
 
 public class Furniture {
 	 String type;
+	 int n =5;
+	 
 	void showtype()
 	{
 		Scanner myobj = new Scanner(System.in);
 		System.out.println("Enter furntiure type");
 		type=myobj.next();
 		System.out.println("Furniture is of type:"+type);
+		System.out.println ("print n");
+	
 	  
 	}
 	public static void main(String[] args) {
