e36862f414ab5fc30144a2b445fcb24222d21c58
rampathak <ram.pathak65@gmail.com>
2024-03-21 20:01:36 -0400

    Added method and object to class

--- src/basics/Furniture.java
+++ src/basics/Furniture.java
@@ -1,8 +1,25 @@
 package basics;
 
+import java.util.Scanner;
+
 public class Furniture {
+	 String type;
+	void showtype()
+	{
+		Scanner myobj = new Scanner(System.in);
+		System.out.println("Enter furntiure type");
+		type=myobj.next();
+		System.out.println("Furniture is of type:"+type);
+	  
+	}
 	public static void main(String[] args) {
 		
+		Furniture furniture =new Furniture();
+		furniture.showtype();
+		
+	
 		System.out.println("we are learning new project");
+		
+		
 	}
 }
